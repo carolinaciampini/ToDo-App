6e4486cf39fa9237b806139d8dea2f7ddbabb3c5
carolinaciampini <140980993+carolinaciampini@users.noreply.github.com>
2024-07-07 19:38:14 -0300

    register page done

--- toDo-Auth/src/App.jsx
+++ toDo-Auth/src/App.jsx
@@ -1,22 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/registerPage"
-
+import { AuthProvider } from "./context/AuthContext"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home page</h1>}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/register" element={<RegisterPage />}></Route>
-        <Route path="/tasks" element={<h1>All tasks page</h1>}></Route>
-        <Route path="/add-task" element={<h1>Create Task page</h1>}></Route>
-        <Route path="/tasks/:id" element={<h1>Update task page</h1>}></Route>
-        <Route path="/profile" element={<h1>Profile page</h1>}></Route>
-        
-     </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="/tasks" element={<h1>All tasks page</h1>}></Route>
+          <Route path="/add-task" element={<h1>Create Task page</h1>}></Route>
+          <Route path="/tasks/:id" element={<h1>Update task page</h1>}></Route>
+          <Route path="/profile" element={<h1>Profile page</h1>}></Route>
+
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+    
  )
 }
 
--- toDo-Auth/src/pages/RegisterPage.jsx
+++ toDo-Auth/src/pages/RegisterPage.jsx
@@ -1,12 +1,19 @@
 import { useForm } from "react-hook-form"
-import { registerRequest } from "../api/auth";
+import { useAuth } from "../context/AuthContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function RegisterPage() {
-  const { register , handleSubmit} = useForm();
+  const { register, handleSubmit, formState:{errors}} = useForm();
+  const { signUp, isAuthenticated } = useAuth()
+  const navigate = useNavigate()
+  
+  useEffect(() => {
+    if(isAuthenticated) navigate('/tasks')
+  },[isAuthenticated])
 
   const onSubmit = handleSubmit(async (values) => {
-    const res = await registerRequest(values);
-    console.log(res)
+    signUp(values)
   })
 
   return (
@@ -14,9 +21,18 @@ function RegisterPage() {
 
       <form onSubmit={onSubmit}>
         
-        <input type="text" {...register("username", { required: true })} className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" placeholder="Username"/>
+        <input type="text" {...register("username", { required: true })} className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" placeholder="Username" />
+        {
+          errors.username && (<p className="text-red-500">An Username is required</p>)
+        }
         <input type="email" {...register("email", { required: true })} className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" placeholder="Email" />
+        {
+          errors.email && (<p className="text-red-500">An email is required</p>)
+        }
         <input type="password" {...register("password", { required: true })} className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" placeholder="Password" />
+        {
+          errors.password && (<p className="text-red-500">A password is required</p>)
+        }
         <button type="submit">Register</button>
 
       </form>
