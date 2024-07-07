a41aecc8abdf71d7b74a4389c85fbbd594aa8608
carolinaciampini <140980993+carolinaciampini@users.noreply.github.com>
2024-07-07 18:29:17 -0300

    create front and validate task

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
   import { authRequired } from '../middlewares/validateToken.js';
 import { createTask, deleteTask, getTask, getTasks, updateTask } from '../controllers/tasks.controller.js';
+import { createTaskSchema } from '../schemas/task.schema.js';
+import { validateSchema } from '../middlewares/validator.middleware.js';
 
 const router = Router();
 
@@ -9,7 +11,7 @@ router.get('/tasks', authRequired, getTasks)
 
 router.get('/tasks/:id', authRequired, getTask)
 
-router.post('/tasks', authRequired, createTask)
+router.post('/tasks', authRequired, validateSchema(createTaskSchema), createTask)
 
 router.delete('/tasks/:id', authRequired, deleteTask)
 
