ef27ebd0933671cc4d89c4eab8e0780cfc4d66cb
carolinaciampini <140980993+carolinaciampini@users.noreply.github.com>
2024-08-20 10:16:38 -0300

    fixing cors

--- src/app.js
+++ src/app.js
@@ -10,10 +10,23 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  'https://to-do-server-peach.vercel.app',
+  'https://to-do-server-carolinas-projects-916ee369.vercel.app',
+  'https://to-do-server-git-main-carolinas-projects-916ee369.vercel.app'
+];
+
 app.use(cors({
-  origin: process.env.API_URL,
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
+
 
 app.use(morgan("dev"));
 app.use(express.json());
